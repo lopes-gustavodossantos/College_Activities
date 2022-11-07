5474a4d094ac5f634c6ed75be68d5d4da44fb0e4
Gustavo dos Santos Lopes <lopes.gustavodossantos@gmail.com>
2022-11-06 22:37:49 -0300

    Add files via upload

---
+++ 2º Semester/Internship/1º Week/Server PHP/Echo/ex1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+
+<head>
+    <title>Impressão de Textos Ex.1</title>
+    <meta charset="utf-8" />
+</head>
+    
+<body>
+    <h1>
+        Crie um documento que exiba a seguinte mensagem “Olá, eu estou aprendendo PHP!”.
+    </h1>
+
+    <div>
+        <?php
+
+        echo "Olá, eu estou aprendendo PHP!";
+
+        ?>
+    </div>
+</body>
+</html>
