0b202ce0f74c628a00898e2ea74675f373251317
Gustavo dos Santos Lopes <lopes.gustavodossantos@gmail.com>
2022-11-06 22:46:12 -0300

    Add files via upload

---
+++ 2º Semester/Internship/1º Week/Server PHP/Variables/ex1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+
+<head>
+    <title>Variaveis Ex.1</title>
+    <meta charset="utf-8" />
+</head>
+    
+<body>
+    <h1>
+        Declare uma variável chamada nome e atribua o seu nome a ela.
+    </h1>
+
+    <div>
+        <?php
+
+        echo $nome = "Gustavo";
+
+        ?>
+    </div>
+</body>
+</html>
---
+++ 2º Semester/Internship/1º Week/Server PHP/Variables/ex2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+
+<head>
+    <title>Variaveis Ex.2</title>
+    <meta charset="utf-8" />
+</head>
+    
+<body>
+    <h1>
+        Declare uma variável que armazene o salário de 3.720.
+    </h1>
+
+    <div>
+        <?php
+
+        echo $salario = number_format(3.720, 3);
+
+        ?>
+    </div>
+</body>
+</html>
